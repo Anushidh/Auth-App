40552409463079f339f13f5ac6db902fbdc807d5
Anushidh <anushidh101@gmail.com>
2024-07-08 01:16:50 +0530

    signup ui complete

---
+++ api/controllers/authController.js
@@ -0,0 +1,15 @@
+import User from '../models/userModel.js'
+import bcryptjs from 'bcryptjs'
+
+
+export const signup = async (req,res, next) => {
+  try {
+    const {username, email, password} = req.body
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+    const newUser = new User({username, email, password: hashedPassword})
+    await newUser.save()
+    res.status(200).json({message: 'user created successfully'})
+  } catch (error) {
+    next(error)
+  }
+}
--- api/index.js
+++ api/index.js
@@ -1,7 +1,33 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import { configDotenv } from "dotenv";
+import userRoutes from './routes/userRoute.js'
+import authRoutes from './routes/authRoute.js'
+configDotenv()
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log('connected to mongodb');
+}).catch((err) => {
+  console.log(err);
+})
 const app = express()
+
+app.use(express.json())
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000')
+})
+
+
+
+app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500
+  const message = err.message || 'Internal Server Error'
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode
+  })
 })
